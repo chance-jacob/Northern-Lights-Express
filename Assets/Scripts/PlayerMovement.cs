152623db3950cf38769ac9e4b74f82105c077487
Chance Jacob <chancejacob@MacBook-Pro-3.local>
2020-12-29 16:57:31 -0700

    Add husky model animations and base movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    public Animator animator;
+    private Vector2 moveDirection;
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInputs();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+        moveSpeed = 0;
+        animator.SetFloat("MovementSpeed", moveSpeed);
+    }
+
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    void Move()
+    {
+        moveSpeed = 3;
+        animator.SetFloat("MovementSpeed", moveSpeed);
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+
+
+}
+
